ad0cabb0eac93194deb06c5c2759a3dab43b150e
Sezgin Kipel <1444sezgin@gmail.com>
2024-03-29 14:31:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const toxicity = require('@tensorflow-models/toxicity');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = 4000;
 let model;
 
 app.use(express.static(path.join(__dirname, 'public')));
